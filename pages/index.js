d216a0887a75dc1e0655d56298db9b0caa3c797a
Jasurbek <jasurisoqov685@gmail.com>
2022-12-06 01:15:30 +0500

    Create Components and files structure

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 
+// Components
+import {
+  Product,
+  Navbar,
+  Layout,
+  HeroBanner,
+  Footer,
+  FooterBanner,
+  Cart,
+} from "../Components";
+
 const Home = () => {
   return (
     <>
       HeroBanner
-      <div>
+      <div className="products-heading">
         <h2>Beset Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
       Footer
     </>
   );
