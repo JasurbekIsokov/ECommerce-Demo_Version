a31a1ff87df08948729abbb59a52813f21dce321
Jasurbek <jasurisoqov685@gmail.com>
2022-12-06 00:36:01 +0500

    change globals style

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      HeroBanner
+      <div>
+        <h2>Beset Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      Footer
+    </>
+  );
 };
 
 export default Home;
