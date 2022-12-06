01494164ef8c0b033aff57e84b98bf525ab65e21
Jasurbek <jasurisoqov685@gmail.com>
2022-12-06 17:14:36 +0500

    Header and Footer component

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
+import { BiPaperPlane } from "react-icons/bi";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>2022 Jasurbek Isokov Headphones All rights reserverd</p>
+      <p className="icons">
+        <AiFillGithub />
+        <AiOutlineTwitter />
+        <BiPaperPlane />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
